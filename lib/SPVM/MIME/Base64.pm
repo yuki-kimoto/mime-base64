227a39137a8bbd6caf22fd03d51728cee7e83763
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-30 07:28:52 +0900

    Add SPVM module

---
+++ lib/SPVM/MIME/Base64.pm
@@ -0,0 +1,49 @@
+package SPVM::MIME::Base64;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::MIME::Base64 - MIME::Base64 is a SPVM module
+
+=head1 Synopsys
+
+  use MIME::Base64;
+  
+=head1 Description
+
+MIME::Base64 is a SPVM module.
+
+=head1 Static Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Bug Report
+
+
+
+=head1 Support
+
+
+
+=head1 Author
+
+
+
+=head1 Copyright & LICENSE
+
+Copyright YYYY-YYYY AUTHOR_NAME, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
